dd9a0c82725ca0c12f9e1d4f0b7931470f93a3d7
csaba patyi <csaba@cloudsolutionarchitect.net>
2021-01-15 20:34:35 +0000

    modified helm to use ClusterIP instead of NodePort. Extracted testing commands to separeate shell script

--- scripts/03_helm_deploy_and_test.sh
+++ scripts/03_helm_deploy_and_test.sh
@@ -8,25 +8,4 @@ echo "##### Deploying helm chart #####"
 helm install $HELM_DEPLOYMENT_NAME ./helm-foobarqix
 
 
-echo "##### Expose the port to localhost from minikube #####"
-export NODE_PORT=$(kubectl get --namespace default -o jsonpath="{.spec.ports[0].nodePort}" services helm-foobarqix)
-export NODE_IP=$(kubectl get nodes --namespace default -o jsonpath="{.items[0].status.addresses[0].address}")
-
-echo " you can run some manual test using the following http://$NODE_IP:$NODE_PORT/<your_input>"
-
-
-echo "##### Running a few auto test (first waiting for 5 sec) #####"
-sleep 5
-
-echo "##### No input test #####"
-curl "http://$NODE_IP:$NODE_PORT"
-
-echo "##### /ready test #####"
-curl "http://$NODE_IP:$NODE_PORT/ready"
-
-echo "##### /1 test #####"
-curl "http://$NODE_IP:$NODE_PORT/1"
-
-echo "##### /53 test #####"
-curl "http://$NODE_IP:$NODE_PORT/53"
 
---
+++ scripts/04_testing_deployed_service.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+HELM_DEPLOYMENT_NAME=helm-foobarqix
+
+echo "##### Starting minikube tunnel in the background if it is not running #####"
+MINIKUBE_TUNNEL_CHECK=$(ps -ef | grep -o "[m]inikube tunnel")
+if [[ "$MINIKUBE_TUNNEL_CHECK" != "minikube tunnel" ]] ; then
+    minikube tunnel &
+    sleep 5
+fi
+
+MINIKUBE_TUNNEL_PID=$(ps -ef | grep  "[m]inikube tunnel" | awk '{ print $2}')
+
+SERVICE_IP=$(kubectl get service $HELM_DEPLOYMENT_NAME |  egrep -v CLUSTER-IP | awk '{ print $3 }')
+SERVICE_PORT=8000
+
+
+echo "##### Running a few auto test (first waiting for 5 sec) #####"
+sleep 5
+
+echo "##### No input test #####"
+curl "http://$SERVICE_IP:$SERVICE_PORT"
+
+echo "##### /ready test #####"
+curl "http://$SERVICE_IP:$SERVICE_PORT/ready"
+
+echo "##### /1 test #####"
+curl "http://$SERVICE_IP:$SERVICE_PORT/1"
+
+echo "##### /53 test #####"
+curl "http://$SERVICE_IP:$SERVICE_PORT/53"
+
